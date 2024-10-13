ee09c186ad3d12d2ae3b810559480204421f82da
GITHUB78600 <githup3@yandex.com>
2024-10-13 17:25:58 +0530

    Update 0_ARIF-RANKUP-1.js

--- scripts/commands/0_ARIF-RANKUP-1.js
+++ scripts/commands/0_ARIF-RANKUP-1.js
@@ -83,7 +83,7 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     let ctx = canvas.getContext("2d");
     ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.rotate(-25 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 2000, 2000, 2000, 2000);
+    ctx.drawImage(baseAvt1, 0, 0, 0, 0);
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
