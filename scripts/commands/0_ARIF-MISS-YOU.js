4e0081a9dbc89c3cad0a8a2672ac240a77ec9073
GITHUB78600 <githup3@yandex.com>
2024-10-13 14:32:34 +0530

    Create 0_ARIF-MISS-YOU.js

---
+++ scripts/commands/0_ARIF-MISS-YOU.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "miss you",
+  version: "1.0.1",
+  permssion: 0,
+  credits: "ARIF BABU", 
+  prefix : true,
+  description: "noprefix gif",
+  category: "no prefix",
+  usages: "bye",
+  cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("miss")==0 || event.body.indexOf("Miss")==0 || event.body.indexOf("MISS")==0 || event.body.indexOf("miss you")==0 || event.body.indexOf("mISS YOU")==0 || event.body.indexOf("MISS YOU")==0) {
+    var msg = {
+        body: "𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😇",
+        attachment: fs.createReadStream(__dirname + `/noprefix/MISS-YOU.gif`)
+      }
+      api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
