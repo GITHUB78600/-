9ac62b1789ff4c8858d9f25d7bd2cb40188c8c6e
GITHUB78600 <githup3@yandex.com>
2024-10-13 17:23:30 +0530

    Update 0_ARIF-RANKUP-1.js

--- scripts/commands/0_ARIF-RANKUP-1.js
+++ scripts/commands/0_ARIF-RANKUP-1.js
@@ -83,7 +83,7 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     let ctx = canvas.getContext("2d");
     ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.rotate(-25 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 40, 710, 630, 700);
+    ctx.drawImage(baseAvt1, 2000, 2000, 2000, 2000);
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
@@ -106,7 +106,7 @@ module.exports.languages = {
     "on": "on",
     "off": "off",
     "successText": "success notification rankup!",
-    "levelup": ",
+    "levelup": "",
   }
 }
 module.exports.run = async function({ api, event, Threads, getText }) {
