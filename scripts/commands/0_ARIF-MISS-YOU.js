7b6e70c2e2952e126e459fac27571ceb65dfc275
GITHUB78600 <githup3@yandex.com>
2024-10-14 08:14:45 +0530

    Update 0_ARIF-MISS-YOU.js

--- scripts/commands/0_ARIF-MISS-YOU.js
+++ scripts/commands/0_ARIF-MISS-YOU.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("miss")==0 || event.body.indexOf("Miss")==0 || event.body.indexOf("MISS")==0 || event.body.indexOf("miss you")==0 || event.body.indexOf("mISS YOU")==0 || event.body.indexOf("MISS YOU")==0) {
+  if (event.body.indexOf("i miss you")==0 || event.body.indexOf("I miss you")==0 || event.body.indexOf("Miss")==0 || event.body.indexOf("MISS")==0 || event.body.indexOf("miss you")==0 || event.body.indexOf("miss")==0 || event.body.indexOf("MISS YOU")==0) {
     var msg = {
         body: "𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😇",
         attachment: fs.createReadStream(__dirname + `/noprefix/MISS-YOU.gif`)
