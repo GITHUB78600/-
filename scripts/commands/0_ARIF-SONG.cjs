89f2fa795d54d42f22d8acdb46a6fc8295c7c69a
GITHUB78600 <githup3@yandex.com>
2024-10-16 06:44:59 +0530

    Update 0_ARIF-SONG.cjs

--- scripts/commands/0_ARIF-SONG.cjs
+++ scripts/commands/0_ARIF-SONG.cjs
@@ -3,7 +3,6 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const ytSearch = require("yt-search");
-return returnPromise
 }
 module.exports.config = {
     name: "song",
