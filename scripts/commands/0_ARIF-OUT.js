1859ec1a273745f03f863522cdc490742704f7d3
GITHUB78600 <githup3@yandex.com>
2024-10-19 21:27:56 +0530

    Update 0_ARIF-OUT.js

--- scripts/commands/0_ARIF-OUT.js
+++ scripts/commands/0_ARIF-OUT.js
@@ -1,20 +1,18 @@
-module.exports.config = {
-   name: "out",
-  version: "1.0.1",
-  permssion: 0,
-  credits: "ARIF BABU", 
-  prefix : true,
-  description: "MADE BY ARIF BABU",
-  category: "BOT ID OUT THE GROUP",
-  usages: "PREFIX",
-  cooldowns: 5, 
-};
+module.exports = {
+  config: {
+  name: "out",
+  version: "1.0.5",
+  credits: "ARIF-BABU",
+  prefix: false,
+  permission: 2,
+  description: "out bot",
+  category: "admin",
+  cooldowns: 5
+},
 
-module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+start: async function({ nayan, events, args }) {
+        if (!args[0]) return nayan.removeUserFromGroup(nayan.getCurrentUserID(), events.threadID);
+  nayan.reply("gd bye", events.threadID)
+        if (!isNaN(args[0])) return nayan.removeUserFromGroup(nayan.getCurrentUserID(), args.join(" "));
 }
-
-module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+}
